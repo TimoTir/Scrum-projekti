132fcbfcea8c2489c7f1b31c84a99ac14b8dddbc
Sanna-Leena Räsänen <sanna-leena.rasanen@student.careeria.fi>
2019-11-04 12:45:22 +0200

    Testi

--- Scrum/Scrum/Controllers/HomeController.cs
+++ Scrum/Scrum/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Scrum.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Titityyy.";
 
             return View();
         }
